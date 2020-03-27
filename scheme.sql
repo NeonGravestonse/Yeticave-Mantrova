cd4b5a329a02cf496e8f668759c5dfead78baffa
NeonGravestonse <62240984+NeonGravestonse@users.noreply.github.com>
2020-03-27 05:08:20 +0300

    Update scheme.sql

--- scheme.sql
+++ scheme.sql
@@ -1,3 +1,64 @@
+Create Table `categories` (
+	`id` int(11) AUTO_INCREMENT PRIMARY KEY, 
+	`title` varchar(45) NOT NULL,
+	`alias` varchar(15) NOT NULL
+) Engine=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE UNIQUE INDEX id ON categories(id);
+
+CREATE Table `lots` (
+	`id` int(11) AUTO_INCREMENT PRIMARY KEY,
+	`name` VARCHAR(255),
+	`description` TEXT,
+	`image_url` varchar(100), 	
+	`start_price` decimal(10,0),
+	`date_create` datetime,
+	`expire_date` datetime,
+	`user_id` int(11),
+	`winner_id` int(11),
+	`category_id` int(11),
+	`bet_price` int(11)
+);
+CREATE FULLTEXT INDEX lot_search ON lots(name, description);
+CREATE INDEX image_url ON lots(image_url);
+CREATE INDEX start_price ON lots(start_price);
+CREATE INDEX date_create ON lots(date_create);
+CREATE INDEX expire_date ON lots(expire_date);
+CREATE INDEX user_id ON lots(user_id);
+CREATE INDEX winner_id ON lots(winner_id);
+CREATE INDEX category_id ON lots(category_id);
+CREATE INDEX bet_price ON lots(bet_price);
+
+
+
+
+
+CREATE Table `bets` (
+	`id` int(11),
+	`bet_date` datetime,
+	`bet_price` decimal(10,0),
+	`user_id` int(11),
+	`lot_id` int(11)
+);
+CREATE UNIQUE INDEX id ON bets(id);
+CREATE INDEX price ON bets(price);
+CREATE INDEX lot_id ON bets(lot_id);
+CREATE INDEX user_id ON bets(user_id);
+
+
+CREATE Table `users` (
+	`id` int(11) AUTO_INCREMENT PRIMARY KEY,
+	`username` varchar(45),
+	`email` varchar(100),
+	`password` varchar(255),
+	`avatar` varchar(100),
+	`red_date` datetime,
+	`contacts` varchar(255)
+); 
+
+CREATE UNIQUE INDEX id ON users(id);
+CREATE UNIQUE INDEX email ON users(email);
+
 ALTER TABLE `lots`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
@@ -46,7 +107,7 @@ ADD CONSTRAINT  `lots_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) O
 ADD CONSTRAINT  `lots_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT  `lots_ibfk_3` FOREIGN KEY (`winner_id`) REFERENCES `category`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-	
-	
+
+
 
 
